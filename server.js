09b7e286b189acf393b1a005b0583d2d6ffaff58
DJone-digi2025 <davidjones.digi.2025@gmail.com>
2026-06-04 13:49:55 +0530

    Remove SMTP verify2

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: email,
     pass: appPassword
